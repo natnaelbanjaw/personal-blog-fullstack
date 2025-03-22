625caaaf380790b7bb0f36179dad87f5e371bd42
Natnael Banjaw <natnaelb382@gmail.com>
2025-03-22 22:27:09 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,18 +2,34 @@ require("dotenv").config();
 
 const express = require("express");
 const expressLayout = require("express-ejs-layouts");
-
+const methodOverride = require("method-override");
 const connectDB = require("./servers/config/db");
+const cookieParser = require("cookie-parser");
+const MongoStore = require("connect-mongo");
+const session = require("express-session");
+
+const { isActiveRoute} = require("./servers/helpers/routeHelpers");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 //connect to db
-
 connectDB();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cookieParser());
+app.use(methodOverride('_method'));
+
+app.use(session({
+    secret:"keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+        mongoUrl: process.env.MONGODB_URL
+})
+}));
+
 
 app.use(express.static("public"));
 
@@ -22,7 +38,11 @@ app.use(expressLayout);
 app.set('layout', './layout/main');
 app.set("view engine", "ejs");
 
+app.locals.isActiveRoute  = isActiveRoute;
+
 app.use("/", require("./servers/routes/main"));
+app.use("/", require("./servers/routes/admin"));
+
 
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}`);
